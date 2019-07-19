6c47b68ec4f48c6a3ff6ea30fb6877d622d5a526
Joseph Adediji <josylado@gmail.com>
2019-07-19 16:35:46 +0300

    add function for toggling first icon

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+ $("#design-pic").click(function(){
+   $(".design-p").toggle();
+   $("#design-img").toggle();
+   $("#design-h").css("font-weight","Bold");
+   $(".design-par").css("text-align","center");
+
+ });
