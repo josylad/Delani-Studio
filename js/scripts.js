cc1ab38032408a71a36db067018b14bab8e46799
Joseph Adediji <josylado@gmail.com>
2019-07-20 12:42:52 +0300

    add image overlay Jquery functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,101 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
- $("#design-pic").click(function(){
-   $(".design-p").toggle();
-   $("#design-icon").toggle();
+  $("#design-pic").click(function() {
+    $(".design-p").toggle();
+    $("#design-pic").toggle();
 
- });
+  });
+  $(".design-p").click(function() {
+    $("#design-pic").show();
+    $(".design-p").hide();
+  })
 
- $("#dev-pic").click(function(){
+
+  $("#dev-pic").click(function() {
     $(".dev-p").toggle();
-    $("#dev-icon").toggle();
+    $("#dev-pic").toggle();
+  });
+  $(".dev-p").click(function() {
+    $("#dev-pic").show();
+    $(".dev-p").hide();
+  })
+
+  $("#product-pic").click(function() {
+    $(".product-p").toggle();
+    $("#product-pic").toggle();
+  });
+  $(".product-p").click(function() {
+    $("#product-pic").show();
+    $(".product-p").hide();
+  })
+
+});
+
+//Mouse overlay effect
+$(document).ready(function() {
+  $('.wrap').mouseover(function() {
+    $('.text-overlay').show();
+  }).mouseout(function() {
+    $('.text-overlay').hide();
   });
 
-  $("#product-pic").click(function(){
-     $(".product-p").toggle();
-     $("#product-img").toggle();
-     $("#product-h").css("font-weight","Bold");
-     $(".product-par").css("text-align","center");
-   });
-
+  $('.wrap2').mouseover(function() {
+    $('.text-overlay2').show();
+  }).mouseout(function() {
+    $('.text-overlay2').hide();
   });
+
+  $('.wrap3').mouseover(function() {
+    $('.text-overlay3').show();
+  }).mouseout(function() {
+    $('.text-overlay3').hide();
+  });
+
+  $('.wrap4').mouseover(function() {
+    $('.text-overlay4').show();
+  }).mouseout(function() {
+    $('.text-overlay4').hide();
+  });
+
+  $('.wrap5').mouseover(function() {
+    $('.text-overlay5').show();
+  }).mouseout(function() {
+    $('.text-overlay5').hide();
+  });
+
+  $('.wrap6').mouseover(function() {
+    $('.text-overlay6').show();
+  }).mouseout(function() {
+    $('.text-overlay6').hide();
+  });
+
+  $('.wrap7').mouseover(function() {
+    $('.text-overlay7').show();
+  }).mouseout(function() {
+    $('.text-overlay7').hide();
+  });
+
+  $('.wrap8').mouseover(function() {
+    $('.text-overlay8').show();
+  }).mouseout(function() {
+    $('.text-overlay8').hide();
+  });
+
+});
+
+// $(document).ready(function() {
+//   $("form").submit(function(event) {
+//     event.preventDefault();
+//     var name = document.forms["myform"]["fname"].value;
+//     var email = document.forms["myform"]["email"].value;
+//     var message = document.forms["myform"]["message"].value;
+//     alert(name + " We have received your message. Thank you for reaching out to us. ");
+//       }
+//   });
+// });
+function contact(form){
+  var name = document.forms["myform"]["fname"].value;
+      var email = document.forms["myform"]["email"].value;
+      var message = document.forms["myform"]["message"].value;
+      alert("Hi " + name + "," + " We have received your message. Thank you for reaching out to us. ");
+        }
